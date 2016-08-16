331752118c9d31e9a8bea013a3fef2936d688f29
metabrez <metabrez@gmail.com>
2016-08-16 10:27:09 -0500

    This is login Example

---
+++ LoginExample/src/com/edu/jee/Login.java
@@ -0,0 +1,35 @@
+package com.edu.jee;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Login extends HttpServlet {
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String userName = request.getParameter("username");
+        String pass = request.getParameter("password");
+        
+        if(Validate.checkUser(userName, pass))
+        {
+            RequestDispatcher rs = request.getRequestDispatcher("Welcome");
+            rs.forward(request, response);
+        }
+        else
+        {
+           out.println("Username or Password incorrect");
+           RequestDispatcher rs = request.getRequestDispatcher("index.html");
+           rs.include(request, response);
+        }
+    }  
+}
---
+++ LoginExample/src/com/edu/jee/Validate.java
@@ -0,0 +1,31 @@
+package com.edu.jee;
+
+import java.sql.*;
+
+public class Validate
+ {
+     public static boolean checkUser(String email,String pass) 
+     {
+      boolean st =false;
+      try{
+
+	 //loading drivers for mysql
+         Class.forName("com.mysql.jdbc.Driver");
+
+ 	 //creating connection with the database 
+         Connection con=DriverManager.getConnection
+                        ("jdbc:mysql://localhost:3306/test","root","engineer");
+         PreparedStatement ps =con.prepareStatement
+                             ("select * from register where username=? and password=?");
+         ps.setString(1, email);
+         ps.setString(2, pass);
+         ResultSet rs =ps.executeQuery();
+         st = rs.next();
+        
+      }catch(Exception e)
+      {
+          e.printStackTrace();
+      }
+         return st;                 
+  }   
+}
